4a13bd504678c0a24f37c9427cc8f5cbe23d9b2c
Amith Abey Stephen <145148320+Amith-Abey-Stephen@users.noreply.github.com>
2023-12-21 11:35:47 +0530

    Update work_light.ino

--- work_light.ino
+++ work_light.ino
@@ -1,6 +1,8 @@
-const int trigPin = 9; 
-const int echoPin = 8; 
-const int relay=7;
+//Initialising the pins
+
+const int trigPin = 9; //trig pin on ultrasonic sensor 
+const int echoPin = 8; //echo pin on ultrasonic sensor 
+const int relay=7;//Data pin on relay
 void setup() {
   Serial.begin(9600);  
   pinMode(trigPin, OUTPUT);
@@ -11,13 +13,18 @@ void setup() {
 void loop() {
   long duration;
   int distance;
+
+  //Trigger the ultrasonic sensor
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+  //Measure the duration of echo signal
   duration = pulseIn(echoPin, HIGH);
+  //calculating the distance in cm
   distance = duration * 0.034 / 2;
+  //Printing in Serial Monitor
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
@@ -30,5 +37,5 @@ void loop() {
   {
     digitalWrite(relay,HIGH);
   }
-  delay(100);
+  delay(100);//delay given before the next measurement
 }
